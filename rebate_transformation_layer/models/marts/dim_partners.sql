8979cfa0a2d0dc8a8be7fe7fdab754f1c5efe1b1
emorain3 <emorain@live.com>
2026-07-13 17:59:28 -0400

    refactor: decoupled partners from affiliates. shoulda caught that sooner

--- rebate_transformation_layer/models/marts/dim_partners.sql
+++ rebate_transformation_layer/models/marts/dim_partners.sql
@@ -1,19 +1,2 @@
-with affiliates as (
-    select * from {{ ref('int_affiliates') }}
-),
 
-partners as (
-    select * from {{ ref('int_partners') }}
-)
-
-select
-    a.affiliate_id,
-    a.affiliate_name,
-    a.parent_id,
-    a.state,
-    a.program_tier,
-    p.partner_id,
-    p.partner_name,
-    p.partner_type
-from affiliates a
-left join partners p on a.parent_id = p.partner_id
+select * from {{ ref('int_partners') }}
